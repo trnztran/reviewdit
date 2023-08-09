4c28a32082340ade40ef3367b63d26dcd7f4c0de
Terrance Tran <t.tranterrance@gmail.com>
2023-08-09 15:31:00 -0700

    get request for amazon product title

--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
+const puppeteer = require('puppeteer');
 const express = require('express');
 const app = express();
 
+const PORT = 5000;
+
 app.get("/api", (req,res) =>{
     res.json({"users": ["userOne", "userTwo", "userThree"]})
-})
+});
 
-app.listen(5000, () => {console.log("sever start on port 5000")})
+app.get("/productName", async (req, res) => {
+    // launch puppeteer to scrape Amazon webpage for product name
+
+    const website = req.query.website;
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(website);
+
+        const [el] = await page.$x('//*[@id="productTitle"]/text()');
+        const txt = await el.getProperty('textContent');
+        const rawTxt = await txt.jsonValue();
+
+        console.log(rawTxt);
+        browser.close();
+
+        return res.status(200).send(rawTxt);
+    } catch (e) {
+        console.log(e);
+        res.status(500).send("Something broke :(");
+    } 
+});
+
+app.listen(PORT, () => {console.log("server start on port 5000")});
