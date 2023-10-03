74c2f6a49662c6aaac8def9ed39ba6e633926576
Terrance Tran <t.tranterrance@gmail.com>
2023-10-02 22:25:34 -0700

    fixed bug where product name did not render

--- src/components/ReviewButton.js
+++ src/components/ReviewButton.js
@@ -1,15 +1,74 @@
 /*global chrome*/
-import React, { Component, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import ReviewListing from './ReviewListing';
 
 function ReviewButton() {
   // const [apiKey, setApiKey] = useState();
-  // const [productName, setProductName] = useState();
-  const [results, setResults] = useState([]);
+  const [productName, setProductName] = useState();
+  // const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentURL, setCurrentURL] = useState();
 
-  // const getAccessToken = async() =>{
+  const getProductName = async (currentURL) => {
+    // fetch request to server with amazon URL
+    // server scrapes the amazon page and returns the product name
+    //product name is stored in productName
+
+      await fetch('http://localhost:5000/productName?website='+currentURL)
+        .then(response => response.text())
+        .then(result => {
+          console.log("result: ",result);
+          setProductName(result);
+          console.log("fetch completed");
+          return(result);
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    }
+
+  const handleClick = async (e) => {
+    //performs all functions needed to grab product name
+
+      e.preventDefault();
+      setIsLoading(true);
+      console.log("start handleclick")
+      
+      await getProductName(currentURL)
+
+      setIsLoading(false);
+
+  }
+
+  useEffect(()=> {
+    // Grabs URL of current active tab and stores in currentURL
+    
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      const currentTab = tabs[0];
+      const url = currentTab.url;
+      setCurrentURL(url);
+    });
+  }, []);
+
+  return(
+      <div>
+        <button onClick = {handleClick} className = "btn">
+          {isLoading ? 'Loading...' : 'Find Reviews'}
+        </button>  
+        <ReviewListing prop = {productName} />
+      </div>
+  )
+}
+
+export default ReviewButton; 
+
+
+
+
+
+
+// const getAccessToken = async() =>{
   // //retrieves accesstoken for reddit API
 
   // // fetch('http://localhost:5000/accessToken')
@@ -31,62 +90,9 @@ function ReviewButton() {
   //   }
   // }
 
-  const getURL = async (callback) => {
-    // callback function to grab current URL that extension is being run on
-
-      return chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-          callback(tabs[0].url)});
-    }
-    
-  const getProductName = async (amazon_URL) => {
-    // fetch request to server with amazon URL
-    // server scrapes the amazon page and returns the product name
-
-      fetch('http://localhost:5000/productName?website='+amazon_URL)
-        .then(response => response.text())
-        .then(result => {
-          console.log(result);
-          return(result);
-        })
-        .catch(err => {
-          console.log(err.message);
-        });
-    }
-
-  const handleClick = async (e) => {
-    //performs all functions needed to grab product name
-
-      e.preventDefault();
-      console.log("start handleclick")
-      try{
-        setIsLoading(true);
-        
-        const productName = await getURL(getProductName);
-        console.log("product name:", productName);
-
-        const allResults = await Promise.all([productName]);
-        console.log("product name:", productName);
-        console.log("allresults:",allResults);
-        setResults(allResults);
-        
-        setIsLoading(false);
-        console.log("end handleclick");
-      } catch(e){
-        console.log('Error:', e);
-        setIsLoading(false);
-      }
-
-  }
-
-
-  return(
-      <div>
-        <button onClick = {handleClick} className = "btn">
-          {isLoading ? 'Loading...' : 'Find Reviews'}
-        </button>  
-        <ReviewListing prop = {results} />
-      </div>
-  )
-}
-
-export default ReviewButton;
+  // const getURL = async (callback) => {
+  //   // callback function to grab current URL that extension is being run on
+  //     console.log("chrome callback");
+  //     return chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+  //         callback(tabs[0].url)});
+  //   }
--- src/components/ReviewListing.js
+++ src/components/ReviewListing.js
@@ -2,19 +2,12 @@ import React from 'react';
 import './App.css';
 
 function ReviewListing({prop}) {
-    console.log("from reviewlisting:", prop);
+
     return (
         <div className = "review-listing">
             <p className='review-search-title'>
-                {/* Showing reviews for 
-                {prop[0]}... */}
-                
-                
-                      <ul>
-        {prop.map((result, index) => (
-          <li key={index}>{result}</li>
-        ))}
-      </ul>
+                Showing reviews for 
+                {prop}...
             </p>
         </div>
     )
