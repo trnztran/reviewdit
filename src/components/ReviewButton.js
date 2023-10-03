e40306bc788b74ea8259cee0454f0f064e6721ff
Terrance Tran <t.tranterrance@gmail.com>
2023-10-03 14:38:30 -0700

    added call to reddit for apikey

--- src/components/ReviewButton.js
+++ src/components/ReviewButton.js
@@ -4,41 +4,56 @@ import './App.css';
 import ReviewListing from './ReviewListing';
 
 function ReviewButton() {
-  // const [apiKey, setApiKey] = useState();
+  const [apiKey, setApiKey] = useState();
   const [productName, setProductName] = useState();
-  // const [results, setResults] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [currentURL, setCurrentURL] = useState();
 
   const getProductName = async (currentURL) => {
     // fetch request to server with amazon URL
     // server scrapes the amazon page and returns the product name
-    //product name is stored in productName
+    // product name is stored in productName
 
-      await fetch('http://localhost:5000/productName?website='+currentURL)
-        .then(response => response.text())
-        .then(result => {
-          console.log("result: ",result);
-          setProductName(result);
-          console.log("fetch completed");
-          return(result);
-        })
-        .catch(err => {
-          console.log(err.message);
-        });
+    await fetch('http://localhost:5000/productName?website='+currentURL)
+      .then(response => response.text())
+      .then(result => {
+        console.log("result: ",result);
+        setProductName(result);
+        console.log("fetch completed");
+        return(result);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }
+ 
+  const getAccessToken = async() =>{
+    // retrieves accesstoken for reddit API
+
+    await fetch('http://localhost:5000/accessToken')
+      .then(response => response.text())
+      .then(result => {
+        console.log("access token: ", result);
+        setApiKey(result);
+        // console.log("access token 2: ", apiKey);
+        return result;
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+      console.log("accesskey:", apiKey);
     }
 
   const handleClick = async (e) => {
     //performs all functions needed to grab product name
 
-      e.preventDefault();
-      setIsLoading(true);
-      console.log("start handleclick")
-      
-      await getProductName(currentURL)
+    e.preventDefault();
+    setIsLoading(true);
+    console.log("start handleclick");
+    
+    await getProductName(currentURL);
 
-      setIsLoading(false);
-
+    setIsLoading(false);
   }
 
   useEffect(()=> {
@@ -49,6 +64,7 @@ function ReviewButton() {
       const url = currentTab.url;
       setCurrentURL(url);
     });
+    getAccessToken();
   }, []);
 
   return(
@@ -56,43 +72,10 @@ function ReviewButton() {
         <button onClick = {handleClick} className = "btn">
           {isLoading ? 'Loading...' : 'Find Reviews'}
         </button>  
+        <p>Access key: {apiKey}</p>
         <ReviewListing prop = {productName} />
       </div>
   )
 }
 
 export default ReviewButton; 
-
-
-
-
-
-
-// const getAccessToken = async() =>{
-  // //retrieves accesstoken for reddit API
-
-  // // fetch('http://localhost:5000/accessToken')
-  // //   .then(response => response.text())
-  // //   .then(result => {
-  // //     console.log(result);
-  // //     return result;
-  // //   })
-  // //   .catch(err => {
-  // //     console.log(err.message);
-  // //   });
-  // try{
-  //     const res = await fetch('http://localhost:5000/accessToken');
-  //     const text = await res.text();
-  //     console.log("does it even reach this point wtf");
-  //     return text;
-  //   } catch(e){
-  //     console.log(e);
-  //   }
-  // }
-
-  // const getURL = async (callback) => {
-  //   // callback function to grab current URL that extension is being run on
-  //     console.log("chrome callback");
-  //     return chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
-  //         callback(tabs[0].url)});
-  //   }
